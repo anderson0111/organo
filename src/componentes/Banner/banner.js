2c300b41e43f27065a5bb0b48ec02cbf1ca48b23
anderson <andesonalmeidask01@gmail.com>
2022-10-08 19:15:40 -0300

    removi arquivos e add novos components

---
+++ src/componentes/Banner/banner.js
@@ -0,0 +1,12 @@
+import './banner.css'
+
+function Banner(){
+    return(
+        <header className="banner">
+            <img src="/imagens/imagemBanner.jpeg" alt="O banner principal da página do organo"/>
+
+        </header>
+    )
+}
+
+export default Banner
